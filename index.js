1fab426d39286e473e29bb93ac728e907fc55b22
jdrichards-pursuit <jdrpursuit@gmail.com>
2022-08-02 09:51:23 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * alwaysTrue()
+ * ---------------------
+ * Always returns the value `true`.
+ * HINT: What data type do we want to return here?
+ * @returns {boolean}
+ *
+ * EXAMPLE:
+ *  alwaysTrue();
+ *  //> true
+ */
+function alwaysTrue() {
+  return true;
+}
+
+/**
+ * greet()
+ * ---------------------
+ * Greets the person by name.
+ * HINT: Look at the example closely.
+ * Capitalization, spacing, spelling, and punctuation count!
+ * @param {string} name
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  greet("Ian");
+ *  //> "Hello, Ian."
+ */
+function greet(name) {
+  return `Hello, ${name}`;
+}
+
+/**
+ * add()
+ * ---------------------
+ * Adds two numbers together and returns the sum.
+ * HINT: If we are adding two numbers, how many parameters do we need?
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  add(10, 20);
+ *  //> 30
+ */
+function add(a, b) {
+  return a + b;
+}
